96f563a4e19589d4e6768624a0b3943bddc87110
alisajid0023 <155763126+alisajid0023@users.noreply.github.com>
2024-01-24 15:29:28 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+// Assignment2
+
+// area of ractangle
+var length = 14;
+var width = 10;
+
+var area = (length*width);
+console.log(area)
+
+
+let userNamed = prompt("Your Name");
+alert("Welcome to my portfolio. Hope you are doing well & you will like my portfolio")
+
+console.log(userNamed);
+// Assignment2
